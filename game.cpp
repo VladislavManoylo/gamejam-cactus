45b3cfef351dbb9ed76c995dc220161efdc7668f
Vladislav Manoylo <vladislav.manoylo@gmail.com>
2016-09-10 16:02:21 -0500

    Set constant fps

--- game.cpp
+++ game.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
-#include <stdlib.h>
+#include <chrono>
+#include <thread>
 #include <ncurses.h>
 using namespace std;
+using namespace std::chrono;
 
 #define DEBUG
 
+const unsigned FPS = 30;
+const unsigned MPF = 1000/FPS;
+
 enum class Command {
 	exit,
-	unknown
+	none
 };
 
 Command getCommand() {
 	char input = getch();
 	switch (input) {
 		case 27: return Command::exit; // 27 is the escape key
-		default: return Command::unknown;
+		default: return Command::none;
 	}
-#ifdef DEBUG
-	cerr << input << '\n';
-#endif
 }
 
 int main() {
 	initscr();
-	Command cmd = Command::unknown;
+	nodelay(stdscr, TRUE);
+	Command cmd = Command::none;
+	time_point<system_clock> time = system_clock::now();
 	do {
 		cmd = getCommand();
-	} while (cmd != Command::exit);
+		time += milliseconds(MPF);
+		this_thread::sleep_until(time);
+#ifdef DEBUG
+		if (cmd == Command::none)
+			cerr << ". ";
+#endif
+		if (cmd == Command::exit)
+			break;
+	} while (true);
 	endwin();
 
 	return 0;
